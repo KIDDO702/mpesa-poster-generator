d4d55f341813497380a3441bd05a8e65061b79cf
Bob <robarangs72@gmail.com>
2025-06-26 15:25:20 +0300

    client-side env

--- client/src/components/Preview/Preview.tsx
+++ client/src/components/Preview/Preview.tsx
@@ -5,6 +5,7 @@ import { FaRegFilePdf } from "react-icons/fa6";
 import { FaRegFileImage } from "react-icons/fa";
 import { ClockLoader, ScaleLoader } from "react-spinners";
 import TemplateSelector from "../Templates/TemplateSelector";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type Props = {
   formData:
@@ -55,7 +56,7 @@ function Preview({ formData, selectedTemplateId, onSelect }: Props) {
 
     try {
       setDownloadStatus("generating");
-      const response = await axios.post("http://localhost:8080/handle-image-download", {
+      const response = await axios.post(`${BASE_URL}/handle-image-download`, {
         formData,
         templateType: formData.templateType,
         selectedTemplateId
@@ -102,7 +103,7 @@ function Preview({ formData, selectedTemplateId, onSelect }: Props) {
     try {
       setPdfDownloadStatus("generating");
       const response = await axios.post(
-        "http://localhost:8080/handle-pdf-download",
+        `${BASE_URL}/handle-pdf-download`,
         {
           formData,
           templateType: formData.templateType,
